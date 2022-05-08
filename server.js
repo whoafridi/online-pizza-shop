c95339e31572c0ce64d622733fc5fd606612a1e1
Md. Rafiuzzaman Bhuiyan Afridi <afridirk10@gmail.com>
2022-05-08 21:55:28 +0600

    done

--- server.js
+++ server.js
@@ -23,21 +23,15 @@ app.use("/api/pizzas", require("./routes/pizzaRoute"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/orders", require("./routes/orderRoute"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-} else {
   app.get("/", (req, res) => {
     res.send("<h1>Hello From Node Server vai nodemon</h1>");
   });
-}
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(
-    `Server Running On ${process.env.NODE_ENV} mode on port no ${process.env.PORT}`
+    `Server Running On mode on port no ${process.env.PORT}`
       .bgMagenta.white
   );
 });
